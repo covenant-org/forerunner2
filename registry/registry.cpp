544b8e876bcaf2e746c13dee4e01ea24b61a6503
Kevin Martinez <kevinvr@hotmail.es>
2025-07-14 12:45:59 -0600

    Opted for single thread router instead of router-dealer model

--- registry/registry.cpp
+++ registry/registry.cpp
@@ -1,30 +1,42 @@
 #include "registry.hpp"
-#include <iostream>
+#include <cstdio>
+#include <optional>
+#include <thread>
+#include <vector>
+#include <zmq.h>
+#include <zmq.hpp>
 
 namespace Core {
-Registry::Registry(int16_t port) : port(port) {}
+Registry::Registry(RegistryConfiguration config)
+    : config(config), ctx(1), router(ctx, ZMQ_ROUTER) {}
+
+void Registry::handle_request() {}
 
 void Registry::run() {
-  zmq::socket_t sock(ctx, zmq::socket_type::router);
   char bind_dir[20];
-  sprintf(bind_dir, "tcp://*:%d", port);
-  sock.bind(std::string(bind_dir));
-  const size_t size = 1024;
-  void *ptr = malloc(size);
-  zmq::mutable_buffer mbuf(ptr, size);
-  while (true){
-    std::optional<zmq::recv_buffer_size> res = sock.recv(mbuf, zmq::recv_flags::none);
-    if (res.has_value()){
-        printf("%ld: ", res.value().size);
-        printf("%s\n", (char *)ptr);
-    }
+  sprintf(bind_dir, "tcp://*:%d", config.port);
+  router.bind(std::string(bind_dir));
+  printf("Listening to %s\n", bind_dir);
+  zmq::message_t identity(10);
+  auto res = router.recv(identity);
+  printf("Received %ld identity %d\n", res.value_or(0),
+         *(uint *)identity.data());
+  zmq::message_t empty(1);
+  res = router.recv(empty);
+  printf("Received %ld empty %d\n", res.value_or(0), *(uint *)empty.data());
+  zmq::message_t data(10);
+  res = router.recv(data);
+  if (res.has_value()) {
+    unsigned received_data = res.value();
+    printf("Received %d data %s\n", received_data, (char *)data.data());
   }
 }
 
 }  // namespace Core
 
-int main(int argc, char** argv) {
-  Core::Registry registry(4020);
+int main(int argc, char **argv) {
+  Core::Registry registry({.port = 4020});
+  printf("help\n");
   registry.run();
   return 0;
 }
--- registry/registry.hpp
+++ registry/registry.hpp
@@ -15,28 +15,34 @@ struct Endpoint {
   uint16_t port;
 };
 
-struct EndpointEfimeral {
+struct EndpointMetadata {
   Endpoint endpoint;
   time_t created_at;
   std::uint8_t ttl;
 };
 
+struct RegistryConfiguration {
+  int16_t port;
+};
+
 class Registry {
-  public:
-  explicit Registry(int16_t port);
+ public:
+  explicit Registry(RegistryConfiguration config);
 
   void run();
-
   void register_topic(const std::string topic, const Endpoint& host);
+
   void deregister_topic(const std::string topic);
 
   std::optional<Endpoint> check_topic(const std::string topic);
-
   void persist_topics();
 
+  void handle_request();
+
  private:
-  int16_t port;
+  RegistryConfiguration config;
   zmq::context_t ctx;
+  zmq::socket_t router;
   std::string _persistance_file;
   std::map<std::string, Endpoint> _topic_to_endpoint;
   std::map<std::string, std::vector<Endpoint>> _topic_to_waiters;
