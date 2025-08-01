feda644c018af79c6a956c4b6acd4401dd99e9c4
Manuelo247 <Manuel8a31@hotmail.com>
2025-08-01 18:29:12 +0000

    corrections

--- launch/launch.hpp
+++ launch/launch.hpp
@@ -1,47 +1,53 @@
 #ifndef LAUNCH_HPP
 #define LAUNCH_HPP
 
-#include <filesystem>
-#include <map>
-#include <string>
-#include <vector>
-#include <thread>
-#include <boost/process.hpp>
-#include <yaml-cpp/yaml.h>
-#include <sstream>
-#include <iostream>
-
-#include "logger.hpp"
 #include "argument_parser.hpp"
+#include "logger.hpp"
 #include "nodes_yaml_parser.hpp"
-
+#include <boost/process.hpp>
+#include <filesystem>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <yaml-cpp/yaml.h>
 
 class Launch {
-protected:
-    Core::Logger _logger;
+ protected:
+  Core::Logger _logger;
 
-private:
-    std::string _root_path;
-    std::vector<std::string> _exclude_folders;
-    inline static const std::vector<std::string> default_exclude_folders = {"vendor", ".git"};
+ private:
+  std::string _root_path;
+  std::vector<std::string> _exclude_folders;
+  inline static const std::vector<std::string> default_exclude_folders = {
+      "vendor", ".git"};
 
-    std::string find_root(const std::string& filename, int max_levels);
-    static std::map<std::string, std::string> find_executable_files(
-        const std::filesystem::path& dir,
-        const std::vector<std::string>& exclude_folders);
+  std::string find_root(const std::string& filename, int max_levels);
+  static std::map<std::string, std::string> find_executable_files(
+      const std::filesystem::path& dir,
+      const std::vector<std::string>& exclude_folders);
 
-public:
-    std::map<std::string, std::string> executables;
+ public:
+  std::map<std::string, std::string> executables;
 
-    void set_log_level(Core::LogLevel level);
+  void set_log_level(Core::LogLevel level);
 
-    Launch(argparse::ArgumentParser& parser, const std::vector<std::string>& exclude, const std::vector<std::string>& names, const std::vector<std::vector<std::string>>& args);
-    Launch(argparse::ArgumentParser& parser, const std::vector<std::string>& names, const std::vector<std::vector<std::string>>& args);
-    Launch(argparse::ArgumentParser& parser, const NodesYamlParser& yaml_parser);
+  Launch(argparse::ArgumentParser& parser,
+         const std::vector<std::string>& exclude,
+         const std::vector<std::string>& names,
+         const std::vector<std::vector<std::string>>& args);
+  Launch(argparse::ArgumentParser& parser,
+         const std::vector<std::string>& names,
+         const std::vector<std::vector<std::string>>& args);
+  Launch(argparse::ArgumentParser& parser, const NodesYamlParser& yaml_parser);
 
-    std::map<std::string, std::string> get_executables();
-    int run_executable(const std::string& name, const std::vector<std::string>& args);
-    void run_executables(const std::vector<std::string>& names, const std::vector<std::vector<std::string>>& arguments);
+  std::map<std::string, std::string> get_executables();
+  int run_executable(const std::string& name,
+                     const std::vector<std::string>& args);
+  void run_executables(const std::vector<std::string>& names,
+                       const std::vector<std::vector<std::string>>& arguments);
 };
 
-#endif // LAUNCH_HPP
+#endif  // LAUNCH_HPP
