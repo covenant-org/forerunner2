09d20707ac7fb0894ef4243aedc0aab30591c3de
Kevin Martinez <kevinvr@hotmail.es>
2025-08-14 00:29:26 +0000

    Build yaml-cpp separate from rest of vendor

--- build_vendor.sh
+++ build_vendor.sh
@@ -5,5 +5,6 @@ set -e
 cd vendor
 cmake -Bbuild -DCMAKE_BUILD_TYPE=Release -DCPPZMQ_BUILD_TESTS=OFF && cmake --build build -j$(nproc) && sudo cmake --install build
 cmake -DCMAKE_BUILD_TYPE=Release -Bbuildarg -Sargparse && sudo cmake --build buildarg -j$(nproc) --target install && sudo ldconfig
+cmake -DCMAKE_BUILD_TYPE=Release -Bbuildyaml -Syaml-cpp && sudo cmake --build buildyaml -j$(nproc) --target install
 cmake -DCMAKE_BUILD_TYPE=Release -Bbuildmav -SMAVSDK && sudo cmake --build buildmav -j$(nproc) --target install && sudo ldconfig
 cmake -DCMAKE_BUILD_TYPE=Release -Bbuildpcl -Spcl && sudo cmake --build buildpcl -j$(nproc) && sudo cmake --install buildpcl
