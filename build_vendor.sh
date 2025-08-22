7be0c0a46bd2737f6014f74be9bd8f796eb069ed
vicman <giltafollavictormanuel@gmail.com>
2025-08-22 14:03:34 -0600

    subscriber for camera topic

--- build_vendor.sh
+++ build_vendor.sh
@@ -7,4 +7,4 @@ cmake -Bbuild -DCMAKE_BUILD_TYPE=Release -DCPPZMQ_BUILD_TESTS=OFF && cmake --bui
 cmake -DCMAKE_BUILD_TYPE=Release -Bbuildarg -Sargparse && sudo cmake --build buildarg -j$(nproc) --target install && sudo ldconfig
 cmake -DCMAKE_BUILD_TYPE=Release -Bbuildyaml -Syaml-cpp && sudo cmake --build buildyaml -j$(nproc) --target install
 cmake -DCMAKE_BUILD_TYPE=Release -Bbuildmav -SMAVSDK && sudo cmake --build buildmav -j$(nproc) --target install && sudo ldconfig
-cmake -DCMAKE_BUILD_TYPE=Release -Bbuildpcl -Spcl && sudo cmake --build buildpcl -j$(nproc) && sudo cmake --install buildpcl
+cmake -DCMAKE_BUILD_TYPE=Release -Bbuildpcl -Spcl && sudo cmake --build buildpcl && sudo cmake --install buildpcl
