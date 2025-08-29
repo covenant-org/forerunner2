91b30d2f3d2c95483971ac36ce0080d367bc7798
Kevin Martinez <kevinvr@hotmail.es>
2025-08-29 17:49:43 -0600

    Docker file build
    Attempt to make it smaller and failling while doing it

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -2,7 +2,5 @@
 sudo apt-get update
 
 # reclaim px4 permissions
-sudo usermod -aG px4 $USER
-git config --global --add safe.directory /usr/local/share/px4
-sudo chown $USER:$USER -R /usr/local/share/px4
-cd /usr/local/share/px4
+git config --global --add safe.directory /tools/PX4-Autopilot
+sudo chown $USER:$USER -R /tools/PX4-Autopilot
