13bef8a71787be133b13ac0880c39f2cfd09a050
Kevin Martinez <kevinvr@hotmail.es>
2025-08-30 05:05:33 +0000

    First draft for the ftp bridge

--- zed/ftp_bridge.cpp
+++ zed/ftp_bridge.cpp
@@ -1,9 +1,37 @@
 #include "ftp_bridge.hpp"
+#include <fstream>
+#include <string>
 
-FtpBridge::FtpBridge(const Core::ArgumentParser& args) {}
+FtpBridge::FtpBridge(const Core::ArgumentParser& args) : Core::Vertex(args) {
+  if (auto uri = args.present("--mavlink-uri")) {
+  } else {
+    this->_ftp_config_sub = this->create_subscriber<KeyValue>(
+        "config/ftp",
+        std::bind(&FtpBridge::config_cb, this, std::placeholders::_1));
+    this->_map_sub = nullptr;
+  }
+}
+
+void FtpBridge::config_cb(const Core::IncomingMessage<KeyValue>& msg) {
+  auto key = msg.content.getKey();
+  if (key != "FTP_DIR") return;
+  this->_ftp_folder = msg.content.getValue();
+  if (this->_map_sub == nullptr) {
+    this->_map_sub = this->create_subscriber<PointCloudChunk>(
+        "map", std::bind(&FtpBridge::map_cb, this, std::placeholders::_1));
+  }
+}
+
+void FtpBridge::map_cb(const Core::IncomingMessage<PointCloudChunk>& msg) {
+  auto id = msg.content.getId();
+  std::ofstream cloudFile(this->_ftp_folder + "/" + std::to_string(id));
+  cloudFile << msg.buffer;
+  cloudFile.close();
+}
 
 int main(int argc, char** argv) {
   Core::ArgumentParser args(argc, argv);
+  args.add_argument("--mavlink-uri");
   FtpBridge bridge(args);
   bridge.run();
 }
--- zed/ftp_bridge.hpp
+++ zed/ftp_bridge.hpp
@@ -9,11 +9,12 @@ class FtpBridge : Core::Vertex {
  private:
   std::shared_ptr<Core::Subscriber<KeyValue>> _ftp_config_sub;
   std::shared_ptr<Core::Subscriber<PointCloudChunk>> _map_sub;
+  std::string _ftp_folder;
 
  public:
   FtpBridge(const Core::ArgumentParser&);
-  void config_cb(const Core::IncomingMessage<KeyValue>);
-  void map_cb(const Core::IncomingMessage<PointCloudChunk>);
+  void config_cb(const Core::IncomingMessage<KeyValue>&);
+  void map_cb(const Core::IncomingMessage<PointCloudChunk>&);
   void run();
 };
 
