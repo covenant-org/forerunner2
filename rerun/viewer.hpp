0ee2a55229922b1615ec8acfd971707e48d46088
Manuelo247 <manuel8a31@hotmail.com>
2025-11-24 16:15:48 -0600

    Viewer without subs and cb

--- rerun/viewer.hpp
+++ rerun/viewer.hpp
@@ -27,24 +27,7 @@ private:
   Core::ArgumentParser _args;
   Core::Logger _logger;
   
-  std::shared_ptr<Core::Subscriber<PointCloud>> _sub;
-  std::shared_ptr<Core::Subscriber<PointCloud>> _map_sub;
-  std::shared_ptr<Core::Subscriber<PointCloudChunk>> _map_chunk_sub;
-  std::shared_ptr<Core::Subscriber<Point>> _goal_sub;
-  std::shared_ptr<Core::Subscriber<StereoMic>> _mic_sub;
-//   std::shared_ptr<Core::Subscriber<Odometry>> _odom_sub;
-  std::shared_ptr<Core::Subscriber<MarkerArray>> _octree_sub;
-  std::shared_ptr<Core::Subscriber<MarkerArray>> _octree_layers_sub;
-  std::shared_ptr<Core::Subscriber<Path>> _planned_path_sub;
-  std::shared_ptr<Core::Subscriber<Path>> _local_planned_path_sub;
-  std::shared_ptr<Core::Subscriber<Point>> _person_reco_path_sub;
-  std::shared_ptr<Core::Subscriber<DetectionImage>> _detection_images_sub;
-  std::shared_ptr<Core::Subscriber<Image>> _zed_image_sub;
-//   std::shared_ptr<Core::Subscriber<Route>> _route_path;
   std::shared_ptr<rerun::RecordingStream> _rec;
-  pcl::io::OctreePointCloudCompression<pcl::PointXYZRGBA> *_point_cloud_decoder;
-  pcl::io::OctreePointCloudCompression<pcl::PointXYZRGBA>
-      *_map_point_cloud_decoder;
   rerun::archetypes::Asset3D _drone_model;
   rerun::Quaternion _drone_quat;
   std::string build_recording_path(
@@ -70,23 +53,8 @@ private:
     std::function<void(const Core::IncomingMessage<T>&)> cb) {
     return this->create_subscriber<T>(topic, cb);
   }
-  void goal_cb(const Core::IncomingMessage<Point> &);
-  void point_cloud_cb(const Core::IncomingMessage<PointCloud> &);
-  void map_cloud_cb(const Core::IncomingMessage<PointCloud> &);
-  void mic_cb(const Core::IncomingMessage<StereoMic> &);
-//   void odom_cb(const Core::IncomingMessage<Odometry> &);
-  void octree_cb(const Core::IncomingMessage<MarkerArray> &);
-  void octree_layers_cb(const Core::IncomingMessage<MarkerArray> &);
-  void planned_path_cb(const Core::IncomingMessage<Path> &);
-  void local_planned_path_cb(const Core::IncomingMessage<Path> &);
-  void person_reco_path_cb(const Core::IncomingMessage<Point> &);
 
   std::shared_ptr<rerun::RecordingStream> get_recording_stream() const { return _rec; }
-  void detection_image_cb(const Core::IncomingMessage<DetectionImage> &);
-  void zed_image_cb(const Core::IncomingMessage<Image> &);
-//   void route_path_cb(const Core::IncomingMessage<Route> &);
-  void render_path(const Core::IncomingMessage<Path> &, const std::string &,
-                   const std::string &);
   void log_map(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud,
                std::string index);
   void map_cloud_chunk_cb(const Core::IncomingMessage<PointCloudChunk> &msg);
